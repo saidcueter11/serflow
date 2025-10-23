2bf7b926197a53378eee109bc37692818210e551
Said Cueter <saidcueter11@gmail.com>
2025-10-22 21:24:59 -0500

    add img product transition

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -5,9 +5,10 @@ interface ProductPreviewProps {
   images: ImageMetadata[]
   mainImage: ImageMetadata
   alt: string
+  id: number
 }
 
-export default function ProductPreview ({ images, mainImage, alt }: ProductPreviewProps) {
+export default function ProductPreview ({ images, mainImage, alt, id }: ProductPreviewProps) {
   const [activeImg, setActiveImg] = useState(mainImage)
   const [hoverImg, setHoverImg] = useState<ImageMetadata | null>(null)
 
@@ -19,6 +20,7 @@ export default function ProductPreview ({ images, mainImage, alt }: ProductPrevi
         src={displayedImg.src}
         alt={alt}
         class="h-80 w-full rounded-lg object-cover object-center aspect-square transition-all duration-300"
+        style={`view-transition-name: img-${id}`}
       />
 
       <div class="flex gap-2 flex-wrap">
@@ -27,8 +29,8 @@ export default function ProductPreview ({ images, mainImage, alt }: ProductPrevi
             key={i}
             type="button"
             class={`w-12 h-12 rounded-md overflow-hidden border-2 transition-all duration-200 ${activeImg.src === img.src
-                ? 'border-accent'
-                : 'border-transparent hover:border-accent'
+              ? 'border-accent'
+              : 'border-transparent hover:border-accent'
               }`}
             onMouseEnter={() => setHoverImg(img)}
             onMouseLeave={() => setHoverImg(null)}
